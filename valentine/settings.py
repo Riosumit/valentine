32bea1c6c7b86ca4e2c7e17567db2ffed153e98e
Riosumit <rajsumit22032003@gmail.com>
2023-02-02 01:28:11 +0530

    propose

--- valentine/settings.py
+++ valentine/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bxp8f1ctwto5#uy+khcum6ol(mj8wrmr!wv8z_+#@6u-7r-)xi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
